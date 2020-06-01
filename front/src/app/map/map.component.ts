cfb0db3a77e0d858a1a83f1f3aebca6532ecf099
cial50233 <cial50233@gmail.com>
2020-06-01 16:10:21 +0200

    marker OK

--- front/src/app/map/map.component.ts
+++ front/src/app/map/map.component.ts
@@ -4,6 +4,7 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import { defaults as defaultControls, OverviewMap, ScaleLine, FullScreen } from 'ol/control';
 import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
 import OSM from 'ol/source/OSM';
 import { fromLonLat } from 'ol/proj';
 import Geolocation from 'ol/Geolocation';
@@ -17,6 +18,8 @@ import VectorLayer from 'ol/layer/Vector';
 import Icon from 'ol/style/Icon';
 import * as olProj from 'ol/proj';
 
+import VectorSource from 'ol/source/Vector';
+
 
 @Component({
   selector: 'app-map',
@@ -24,47 +27,106 @@ import * as olProj from 'ol/proj';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
- public map;
-  view = new View({
+  test;
+  private map;
+  view;
+  /*view = new View({
     center: olProj.fromLonLat([2.5838853999999998, 48.84416820000001]),
     zoom: 15,
     minZoom: 3,
     maxZoom: 19,
-  });
-
+  });*/
+  geolocation;
+  //markers
+  markers;
+  vectorSource;
+  vectorLayer;
   //map
 
   lat: number;
   lon: number;
-  address: string;
-  private geoCoder;
 
+  latitude: number;
+  longitude: number;
 
+  precision: number;
+  position: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.setCurrentLocation2();
+
+
     this.initializeMap();
-
-
+/*
     setTimeout(() => {
       this.setCurrentLocation();
     }, 500);
-
-    //this.setCurrentLocation2()
+*/
+    this.test = "ntin";
   }
 
   initializeMap() {
-    var source = new OSM();
+    // Objet géographique de la position de géolocalisation
+    var ObjPosition = new Feature();
+    // Attribution d'un style à l'objet
+    ObjPosition.setStyle(new Style({
+      image: new CircleStyle({
+        radius: 6,
+        fill: new Fill({
+          color: '#3399CC'
+        }),
+        stroke: new Stroke({
+          color: '#fff',
+          width: 2
+        })
+      })
+    }));
+    this.markers = new Feature({
+      geometry: new Point(fromLonLat([2.598533, 48.840073]))
+    });
+
+    this.markers.setStyle(new Style({
+      image: new Icon(({
+        color: '#8959A8',
+        crossOrigin: 'anonymous',
+        src: 'assets/dot.png',
+        imgSize: [30, 30]
+      }))
+    }));
+
+    this.vectorSource = new VectorSource({
+      features: [ObjPosition,this.markers]
+    });
+
+    this.vectorLayer = new VectorLayer({
+      source: this.vectorSource
+    });
+    this.map = new Map({
+      target: 'carte',
+      layers: [ new TileLayer({
+        source: new OSM()
+      }), this.vectorLayer ],
+      view: new View({
+        center: fromLonLat([2.5838853999999998, 48.84416820000001]),
+        zoom: 16
+      })
+    });
+
+    //var source = new OSM();
+    var source = new XYZ({
+      url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    });
     var overviewMapControl = new OverviewMap({
       layers: [
         new TileLayer({
           source: source
-        })
+        }) 
       ]
     });
 
-    this.map = new Map({
+    let map = new Map({
       controls: defaultControls().extend([
         overviewMapControl,
         new ScaleLine(),
@@ -73,11 +135,11 @@ export class MapComponent implements OnInit {
       layers: [
         new TileLayer({
           source: source
-        })
-      ],
+        })],
       target: 'carte',
       view: this.view
     });
+    this.map = map;
   }
 
   // Get Current Location Coordinates
@@ -112,15 +174,18 @@ export class MapComponent implements OnInit {
     });
     // On scrute les changements des propriétés
     geolocation.on('change', function (evt) {
-      var precision = geolocation.getAccuracy();
-
-      var position = geolocation.getPosition();
+      let precision;
+      precision = geolocation.getAccuracy();
+      let position;
+      position = geolocation.getPosition();
       // On transforme la projection des coordonnées
       var newPosition = transform(position, 'EPSG:3857', 'EPSG:4326');
-      this.lat = newPosition[1];
-      this.lon = newPosition[0];
+      this.latitude = newPosition[1];
+      this.longitude = newPosition[0];
       // Attribution de la géométrie de ObjPosition avec les coordonnées de la position
       ObjPosition.setGeometry(position ? new Point(position) : null);
+      this.precision = precision;
+      this.position = position;
     });
     // On alerte si une erreur est trouvée
     geolocation.on('error', function (erreur) {
@@ -128,24 +193,41 @@ export class MapComponent implements OnInit {
     });
     // Source du vecteur contenant l'objet géographique
     var sourceVecteur = new Vector({
-      features: [ObjPosition]
+      features: [ObjPosition, this.markers]
     });
     // Couche vectorielle 
-    var vecteur = new VectorLayer({
+    new VectorLayer({
       map: this.map,
       source: sourceVecteur
     });
     // Zoom sur l'emprise du vecteur
-    sourceVecteur.once('change', function (evt) {
+    
+ /*   sourceVecteur.once('change', function (evt) {
+      var map = new Map({
+        target: 'carte',
+        layers: [
+          new TileLayer({
+            source: this.source,
+          })
+        ],
+        view: new View({
+          center: [0, 0],
+          zoom: 2
+        })
+      });;
       // On vérifie que la source du vecteur sont chargés
       if (sourceVecteur.getState() === 'ready') {
         // On vérifie que le veteur contient au moins un objet géographique
         if (sourceVecteur.getFeatures().length > 0) {
           // On adapte la vue de la carte à l'emprise du vecteur
-          this.map.getView().fit(sourceVecteur.getExtent(), this.map.getSize());
+          // map.getView().fit(sourceVecteur.getExtent(), map.getSize());
+          map.getView().fit(sourceVecteur.getExtent());
         }
       }
-    });
+      this.map = map;
+    });*/
+
+   // this.geolocation;
   }
 
 }
