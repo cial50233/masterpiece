e97fe2a629f4154024bd7090c6da524aa69f3fc2
Cial TRIN <cial50233@gmail.com>
2020-07-10 21:07:34 +0200

    front: ad page in progress

--- front/src/app/ad/ad.component.ts
+++ front/src/app/ad/ad.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-ad',
@@ -15,12 +17,33 @@ export class AdComponent implements OnInit {
 
   public animalTypes: string[];
 
-  constructor() {
-    this.animalTypes = ["Dog", 'Cat', 'Fish', 'Farm', 'Exotic light', 'Exotic warn'];
+  public animals: any[] = [{
+    id: 1,
+    animalType: '',
+    animalName: '',
+    comment: ''
+  }];
 
+  constructor(private formBuilder: FormBuilder,
+    private _location: Location) {
+    this.animalTypes = ["Dog", 'Cat', 'Fish', 'Farm', 'Exotic light', 'Exotic warn'];
+    this.adForm = this.formBuilder.group({
+      adr: '',
+      animalType: '',
+      animalName: '',
+      comment: ''
+    });
 
   }
   ngOnInit(): void {
+  }
+
+  onReset() {
+    this.adForm.reset();
+  }
+
+  onReturn() {
+    this._location.back();
   }
   onRadioChange(evt) {
     var target = evt.target;
@@ -37,6 +60,24 @@ export class AdComponent implements OnInit {
     } else {
       console.log('chedzzzz');
     }
+  }
+
+  addAnimal(){
+    this.animals.push({
+      id: this.animals.length + 1,
+      animalType: '',
+      animalName: '',
+      comment: ''
+    });
+
+  }
+
+  removeAnimal(i: number) {
+    this.animals.splice(i, 1);
+  }
+
+  submit(){
+    console.log(this.animals)
   }
 
 }
