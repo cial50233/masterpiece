25b86518de23e420b7f3e8f2ec8ccd0312700df6
Cial TRIN <cial50233@gmail.com>
2020-09-03 04:15:15 +0200

    add JSON file way to bind data

--- front/src/app/services/data.service.ts
+++ front/src/app/services/data.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
       private REST_API_SERVER = "http://localhost:8081/announcements";
-
+      //private REST_API_SERVER = "assets/announcements.json";
       constructor(private httpClient: HttpClient) { }
 
       public sendGetRequest() {
