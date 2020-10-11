be343d9221d0b7469601dc3ad576d89fcda00386
Cial TRIN <cial50233@gmail.com>
2020-10-11 13:10:58 +0200

    add dialog box to logout button

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -28,19 +28,23 @@ export class AppComponent {
   setLang(language: string) {
     this.translate.use(language);
   }
-/*
+
   private tokenExpired(token: string) {
     const expiry = (JSON.parse(atob(token.split('.')[1]))).exp;
     return (Math.floor((new Date).getTime() / 1000)) >= expiry;
   }
-*/
+
   ngOnInit() {
-  /*  if (this.tokenExpired(sessionStorage.getItem("accessToken"))) {
-      this.authenticationService.isLogged();
-      console.log("Token has expired");
-    } else {
-      // token valid
-    }*/
+    if (sessionStorage.getItem("accessToken")) {
+      if (this.tokenExpired(sessionStorage.getItem("accessToken"))) {
+        this.logout();
+        console.log("Token has expired");
+      } else {
+        const expiry = (JSON.parse(atob(sessionStorage.getItem("accessToken").split('.')[1]))).exp;
+
+        console.log("Token has valid expires in : " + (expiry - Math.floor((new Date).getTime() / 1000)) + " sec");
+      }
+    }
   }
 
   isLogged() {
@@ -48,7 +52,16 @@ export class AppComponent {
   }
 
   logout() {
-    this.authenticationService.logout();
+    //this.authenticationService.logout();
+    const dialogRef = this.dialog.open(DialogBoxConfirm, {
+      width: '350px',
+      data: { username: this.username, email: this.email }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+
+    });
   }
 
   getUserInfo() {
@@ -71,12 +84,14 @@ export class AppComponent {
       })
       .catch(function (error) {
         console.log(error);
+        self.username = "Not connected";
+        self.email = "Not connected";
+        self.logout();
       });
 
 
     const dialogRef = this.dialog.open(DialogBox, {
-      width: '350px',
-      data: {username: this.username, email: this.email}
+      width: '350px'
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -94,9 +109,31 @@ export class DialogBox {
 
   constructor(
     public dialogRef: MatDialogRef<DialogBox>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
   onOkClick(): void {
+    this.dialogRef.close();
+  }
+
+}
+
+@Component({
+  selector: 'dialog-box-confirm',
+  templateUrl: 'dialog-box-confirm.html',
+})
+export class DialogBoxConfirm {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogBoxConfirm>,
+    private authenticationService: AuthenticationService) { }
+
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
+
+  
+  onOkClick(): void {
+    this.authenticationService.logout();
     this.dialogRef.close();
   }
 
