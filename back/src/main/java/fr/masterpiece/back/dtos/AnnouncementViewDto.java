500df1576f3af495a93021d78cb425980871f21f
Cial TRIN <cial50233@gmail.com>
2021-03-20 11:27:47 +0100

    add contact field at viewAd page

--- back/src/main/java/fr/masterpiece/back/dtos/AnnouncementViewDto.java
+++ back/src/main/java/fr/masterpiece/back/dtos/AnnouncementViewDto.java
@@ -38,6 +38,16 @@ public class AnnouncementViewDto {
     private LocalDateTime lastUpDate;
 	
 	private String username;
+	
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 	public String getUsername() {
 		return username;
