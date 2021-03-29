7c608deae5b3c5939528d2711020e21e6265442a
Cial TRIN <cial50233@gmail.com>
2021-03-29 15:53:49 +0200

    Create-account : Change saveUser to axios

--- front/src/app/create-account/create-account.component.ts
+++ front/src/app/create-account/create-account.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient} from '@angular/common/http';
 import { ValidationService } from './../services/validation.service';
 import { Location } from '@angular/common';
+import { AuthenticationService } from 'src/app/services/authentication.service'
 
 @Component({
   selector: 'app-create-account',
@@ -11,12 +12,13 @@ import { Location } from '@angular/common';
 })
 export class CreateAccountComponent implements OnInit {
 
-  edited = false;
+  edited = this.authenticationService.isLogged();
   submitted = false;
   profilForm: FormGroup;
   errorMsg = "";
 
-  constructor(private formBuilder: FormBuilder,private httpClient: HttpClient,private _location: Location){
+  constructor(private formBuilder: FormBuilder,private httpClient: HttpClient,private _location: Location,
+    private authenticationService: AuthenticationService){
     this.profilForm = this.formBuilder.group({
       username: ['', [Validators.required, ValidationService.userNameValidator]],
       email: ['', [Validators.required, ValidationService.emailValidator]],
@@ -25,31 +27,6 @@ export class CreateAccountComponent implements OnInit {
   }
 
   ngOnInit() {
-  }
-
-  public saveUser() {
-    this.submitted = true;
-    this.httpClient
-      .post('http://localhost:8081/api/accounts/create/', this.profilForm.value)
-      .subscribe(
-        (data) => {
-          console.log(data);
-          document.getElementById("alertMsg").setAttribute("style", "display:block;");
-          this.errorMsg = "Registered done";
-          document.getElementById("alertMsg").classList.add("alert-success");
-          document.getElementById("alertMsg").classList.remove('alert-danger');
-          this.edited = true;
-          this.goLog(this.profilForm.value.username, this.profilForm.value.password);
-        },
-        (error) => {
-          console.log(error);
-          document.getElementById("alertMsg").setAttribute("style", "display:block;");
-          this.errorMsg = "User already registered";
-          document.getElementById("alertMsg").classList.add('alert-danger');
-          document.getElementById("alertMsg").classList.remove("alert-success");
-        }
-      );
-
   }
 
   onReset() {
@@ -61,6 +38,35 @@ export class CreateAccountComponent implements OnInit {
 
   onReturn() {
     this._location.back();
+  }
+
+  public saveUser() {
+    this.submitted = true;
+    const url = 'http://localhost:8081/api/accounts/create/';
+    const axios = require('axios');
+    var config = {
+      method: 'post',
+      url: url,
+      data: this.profilForm.value
+    };
+    let self = this;
+    axios(config)
+      .then(function (response) {
+        console.log(response);
+          document.getElementById("alertMsg").setAttribute("style", "display:block;");
+          self.errorMsg = "Registered done";
+          document.getElementById("alertMsg").classList.add("alert-success");
+          document.getElementById("alertMsg").classList.remove('alert-danger');
+          self.edited = true;
+          self.goLog(self.profilForm.value.username, self.profilForm.value.password);
+      })
+      .catch(function (error) {
+        console.log(error);
+        document.getElementById("alertMsg").setAttribute("style", "display:block;");
+        self.errorMsg = "User already registered";
+          document.getElementById("alertMsg").classList.add('alert-danger');
+          document.getElementById("alertMsg").classList.remove("alert-success");
+      });
   }
 
   goLog(username, password){
@@ -84,15 +90,11 @@ export class CreateAccountComponent implements OnInit {
 
     axios(config)
       .then(function (response) {
-        console.log(JSON.stringify(response.data));
         window.sessionStorage.setItem("accessToken", response.data.access_token);
         console.log("Connexion réussie");
-        console.log(sessionStorage.getItem("accessToken"));
       })
       .catch(function (error) {
         console.log(error);
       });
-    
   }
-
 }
